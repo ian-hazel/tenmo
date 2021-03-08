1c193c7dec6210aa586189af28f9ecf092940567
Ian Hazel <ian.hazel@gmail.com>
2021-03-07 18:04:21 -0600

    added TODO to implement methods to change balance

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDAO.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDAO.java
@@ -5,5 +5,9 @@ import java.math.BigDecimal;
 public interface AccountDAO {
 
 	BigDecimal getBalance(String username);
+	
+	// TODO: implement change balance method
+	
+	// TODO: implement accountDao in transfer controller
 		
 }
